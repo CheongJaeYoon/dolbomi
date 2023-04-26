cd8bf5420d8f80bb7b6566302eefbdcaaa57ea8e
Cheong JaeYoon <jyjung9910@gmail.com>
2023-04-26 22:24:13 +0900

    init

---
+++ src/main/java/com/example/dolbomi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.dolbomi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DolbomiApplication.class);
+    }
+
+}
